8749108bba1c94aa684abcbba33b5592b80ebe10
Levodeanschi Anastasia <levodeanschi.anastasia@gmail.com>
2025-03-22 11:18:19 +0200

    Atestare

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
+import images from './data/images.json'
+import {useState} from 'react';
+
 function App() {
-    return <></>;
+    const [imageurl, setImage] = useState(images);
+    const [selectedIndex, setIndex] = useState(null);
+    //const [count, setCount] = useState(0);
+
+    const ImageButton = ({ name, onClick}) => {
+        return (
+            <button onClick={onClick}  >
+                <img src={"src/assets/question.svg"} alt={name}/>
+            </button>
+    
+        )
+    }
+
+
+    const select = (index) => {
+        setIndex(index);
+    }
+
+
+    const open = () => {
+        setImage();
+    }
+    
+    return  (
+    <>
+    <div>
+       
+       {selectedIndex !== null && imageurl[selectedIndex] && (
+         <img src={imageurl[selectedIndex].url} alt="selected"/>
+            
+        )
+        }
+    </div>
+
+    <div>
+        {imageurl.map((item, index) => (
+            <ImageButton key={item.id} src={item.url} onClick={() => select(index)} />
+        ))}
+         {imageurl.map((item, index) => (
+            <ImageButton key={item.id} src={item.url} onClick={() => select(index)} />
+        ))}
+         {imageurl.map((item) => (
+            <ImageButton key={item.id} src={item.url} onClick={() => open()} />
+        ))}
+    </div>
+
+
+    </>);
 }
 
 export default App;
